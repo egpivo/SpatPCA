bfe487e2be1964723a75d4808907cae0e51d82cd
Wen-Ting Wang <egpivo@gmail.com>
2020-12-30 20:28:29 +0800

    CHG: reformat code

--- tests/testthat/test-SpatPCA.R
+++ tests/testthat/test-SpatPCA.R
@@ -1,8 +1,11 @@
 # generate 1-D data with a given seed
 set.seed(1234)
 x_1D <- as.matrix(seq(-5, 5, length = 10))
-Phi_1D <- exp(-x_1D^2) / norm(exp(-x_1D^2), "F")
-Y_1D <- rnorm(n = 100, sd = 3) %*% t(Phi_1D) + matrix(rnorm(n = 100 * 10), 100, 10)
+Phi_1D <- exp(-x_1D ^ 2) / norm(exp(-x_1D ^ 2), "F")
+Y_1D <- {
+  rnorm(n = 100, sd = 3) %*% t(Phi_1D) +
+    matrix(rnorm(n = 100 * 10), 100, 10)
+}
 cv_1D <- spatpca(x = x_1D, Y = Y_1D)
 
 # Test the result
