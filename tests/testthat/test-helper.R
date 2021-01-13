c7041ff9eb355938c15839babcb4d6d63aa4a640
Wen-Ting Wang <egpivo@gmail.com>
2021-01-13 21:34:15 +0800

    CHG: modify the test to enhance coverage

--- tests/testthat/test-helper.R
+++ tests/testthat/test-helper.R
@@ -4,7 +4,8 @@ test_that("The number of cores for RcppParallel", {
                "Please enter valid type - but got character")
   expect_error(set_cores(0),
                "The number of cores is not greater than 1 - but got 0")
-  expect_true(set_cores(2))
+  expect_error(set_cores(defaultNumber + 1),
+               cat("The input number of cores is invalid - default is ", defaultNumber))
   expect_true(set_cores(defaultNumber))
   expect_null(set_cores())
 })
