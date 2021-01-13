79b55c2c5fd79642364052e2adcc021b18a81d2e
Wen-Ting Wang <egpivo@gmail.com>
2021-01-13 21:24:55 +0800

    CHG: change the test input

--- tests/testthat/test-helper.R
+++ tests/testthat/test-helper.R
@@ -4,7 +4,7 @@ test_that("The number of cores for RcppParallel", {
                "Please enter valid type - but got character")
   expect_error(set_cores(0),
                "The number of cores is not greater than 1 - but got 0")
-  expect_true(set_cores(3))
+  expect_true(set_cores(2))
   expect_true(set_cores(defaultNumber))
   expect_null(set_cores())
 })
