9ea74b2a082696e4c4d52c0e9b45a0dcb084b4fd
Wen-Ting Wang <egpivo@gmail.com>
2021-01-17 20:53:35 +0800

    CHG: reformat R code by styler

--- R/helper.R
+++ R/helper.R
@@ -7,19 +7,25 @@
 #' @keywords internal
 #' @param ncores Number of number of cores for parallel computing. Default is NULL.
 #' @return Logical
-#' 
+#'
 set_cores <- function(ncores = NULL) {
   if (!is.null(ncores)) {
-    if (!is.numeric(ncores))
+    if (!is.numeric(ncores)) {
       stop("Please enter valid type - but got ", class(ncores))
-    
+    }
+
     defaultNumber <- RcppParallel::defaultNumThreads()
-    if (ncores > defaultNumber)
+    if (ncores > defaultNumber) {
       stop("The input number of cores is invalid - default is ", defaultNumber)
-    if (ncores < 1)
+    }
+    if (ncores < 1) {
       stop("The number of cores is not greater than 1 - but got ", ncores)
-    tryCatch({
-      RcppParallel::setThreadOptions(numThreads = ncores)
-    }, error = print)
+    }
+    tryCatch(
+      {
+        RcppParallel::setThreadOptions(numThreads = ncores)
+      },
+      error = print
+    )
   }
 }
