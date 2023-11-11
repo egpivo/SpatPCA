88ddbd532920007f2265e7a0935f96c36d9f1dfc
egpivo <egpivo@gmail.com>
2023-11-11 15:16:22 +0800

    CHG: increase the coverage

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -14,3 +14,40 @@ test_that(".onAttach sets RCPP_PARALLEL_BACKEND and displays welcome message", {
   # Restore the original value of RCPP_PARALLEL_BACKEND
   Sys.setenv(RCPP_PARALLEL_BACKEND = original_backend)
 })
+
+test_that(".onAttach works without interfering with CRAN submission", {
+  # Simulate CRAN submission by setting NOT_CRAN to true
+  Sys.setenv(NOT_CRAN = "true")
+  
+  # Save the current value of RCPP_PARALLEL_BACKEND
+  original_backend <- Sys.getenv("RCPP_PARALLEL_BACKEND")
+  
+  # Call the .onAttach function
+  .onAttach("SpatPCA", "SpatPCA")
+  
+  # Check if RCPP_PARALLEL_BACKEND is not set when NOT_CRAN is true
+  expect_equal(Sys.getenv("RCPP_PARALLEL_BACKEND"), original_backend)
+  
+  # Reset NOT_CRAN to avoid interference with other tests
+  Sys.unsetenv("NOT_CRAN")
+})
+
+test_that(".onAttach sets RCPP_PARALLEL_BACKEND for parallel processing", {
+  # Save the current value of RCPP_PARALLEL_BACKEND
+  original_backend <- Sys.getenv("RCPP_PARALLEL_BACKEND")
+  
+  # Simulate parallel processing by setting NOT_CRAN to false
+  Sys.setenv(NOT_CRAN = "false")
+  
+  # Call the .onAttach function
+  .onAttach("SpatPCA", "SpatPCA")
+  
+  # Check if RCPP_PARALLEL_BACKEND is set to "tinythread" for parallel processing
+  expect_equal(Sys.getenv("RCPP_PARALLEL_BACKEND"), "tinythread")
+  
+  # Restore the original value of RCPP_PARALLEL_BACKEND
+  Sys.setenv(RCPP_PARALLEL_BACKEND = original_backend)
+  
+  # Reset NOT_CRAN to avoid interference with other tests
+  Sys.unsetenv("NOT_CRAN")
+})
